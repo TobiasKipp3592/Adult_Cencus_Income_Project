9fe80cd03cf943521704fd556b5e042281a866e2
Tobias Kipp <tobias.kipp@gmx.net>
2025-02-26 23:25:51 +0100

    build an EDA app with llm

---
+++ app_EDA.py
@@ -0,0 +1,126 @@
+import os 
+import pandas as pd
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_community.llms import OpenAI
+from langchain_experimental.agents import create_pandas_dataframe_agent
+
+# openai api
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# Title
+st.title("AI Assistant for Data Science 🤖")
+
+# Welcoming message
+st.write("Hello! 👋 Welcome to the AI Assistant for Data Science. This tool is designed to help you perform Exploratory Data Analysis on your dataset. Simply upload your CSV file, and let our AI guide you through the insights. Get ready to uncover hidden patterns and make data-driven decisions!")
+
+# Explanation sidebar
+with st.sidebar:
+    
+    st.write("*Your Data Science Adventure Starts Here!*")
+    st.caption("""**Here, you can effortlessly upload your CSV files and begin your data exploration journey.
+    Our tool is designed to help you perform comprehensive Exploratory Data Analysis with ease.
+    Simply upload your dataset, and let our AI guide you through the insights.
+    Get ready to uncover hidden patterns and make data-driven decisions!
+    You can upload your CSV file here.**""")
+
+    st.divider()
+
+    st.caption("<p style='text-align:center'>Made By Tobias Kipp</p>", unsafe_allow_html=True)
+
+# Initialize the key in session state
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = {1:False}
+
+# Function to update the value in session state
+def clicked(button):
+    st.session_state.clicked[button] = True
+st.button("Let's get started", on_click= clicked, args=[1])
+if st.session_state.clicked[1]:
+    user_csv = st.file_uploader("Upload your CSV file here", type=["csv"])
+    if user_csv is not None:
+        user_csv.seek(0)
+        df = pd.read_csv(user_csv, low_memory=False)
+
+        # llm model
+        llm = OpenAI(temperature=0)
+
+        # Function sidebar
+        @st.cache_data
+        def steps_eda():
+            steps_eda = llm("What are the steps of EDA")
+            return steps_eda
+
+        # Pandas Agent
+        pandas_agent = create_pandas_dataframe_agent(llm, df, verbose=True, allow_dangerous_code=True)
+
+        # Functions main
+        @st.cache_data
+        def function_agent():
+            st.write("**Data Overview**")
+            st.write("The first rows of your dataset look like this:")
+            st.write(df.head())
+            st.write("**Data Cleaning**")
+            columns_df = pandas_agent.run("What are the meaning of the columns?")
+            st.write(columns_df)
+            missing_values = pandas_agent.run("How many missing values does this dataframe have? Start the answer with 'There are'")
+            st.write(missing_values)
+            duplicates = pandas_agent.run("Are there any duplicate values and if so where?")
+            st.write(duplicates)
+            st.write("**Data Summarisation**")
+            st.write(df.describe())
+            correlation_analysis = pandas_agent.run("Calculate correlations between numerical variables to identify potential relationships.")
+            st.write(correlation_analysis)
+            outliers = pandas_agent.run("Identify outliers in the data that may be erroneous or that may have a significant impact on the analysis.")
+            st.write(outliers)
+            new_features = pandas_agent.run("What new features would be interesting to create?.")
+            st.write(new_features)
+            return
+
+        @st.cache_data
+        def function_question_variable():
+            st.line_chart(df, y = [user_question_variable])
+            summary_statistics = pandas_agent.run(f"What are the mean, median, mode, standard deviation, variance, range, quartiles, skewness and kurtosis of {user_question_variable}")
+            st.write(summary_statistics)
+            normality = pandas_agent.run(f"Check for normality or specific distribution shapes of {user_question_variable}")
+            st.write(normality)
+            outliers = pandas_agent.run(f"Assess the presence of outliers of {user_question_variable}")
+            st.write(outliers)
+            trends = pandas_agent.run(f"Analyse trends, seasonality, and cyclic patterns of {user_question_variable}")
+            st.write(trends)
+            missing_values = pandas_agent.run(f"Determine the extent of missing values of {user_question_variable}")
+            st.write(missing_values)
+            return
+
+        @st.cache_data
+        def function_question_dataframe():
+            dataframe_info = pandas_agent.run(user_question_dataframe)
+            st.write(dataframe_info)
+            return 
+
+        # Main
+
+        st.header("Exploratory Data Analysis")
+        st.subheader("General information about the dataset")
+
+        with st.sidebar:
+            with st.expander("What are the steps of EDA"):
+                st.write(steps_eda())
+
+        function_agent()
+
+        st.subheader("Variable of study")
+
+        user_question_variable = st.text_input("What variable are you interested in")
+        if user_question_variable is not None and user_question_variable != "":
+            function_question_variable()
+        
+            st.subheader("Further study")
+
+        if user_question_variable:
+            user_question_dataframe = st.text_input( "Is there anything else you would like to know about your dataframe?")
+            if user_question_dataframe is not None and user_question_dataframe not in ("","no","No"):
+                    function_question_dataframe()
+            if user_question_dataframe in ("no", "No"):
+                    st.write("")
